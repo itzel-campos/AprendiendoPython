3ac28e80e32b33e5cb571616c5ee8d3ddb8d1eaf
itzel-campos <55063781+itzel-campos@users.noreply.github.com>
2019-09-17 21:50:59 -0500

    Update Conversiones.py

--- Conversiones.py
+++ Conversiones.py
@@ -1,13 +1,13 @@
 #Itzel Campos
 #16/09/2019
-#Se declara una variable str, con una cadena de digitos
+#Se le da un valor de cadena con una serie de numeros
 Numero = "1234"
 #Se muestra el tipo de que tiene la variable.
 print (type(Numero))
-#Se convierte a entero
+#Se cambia el tipo a entero a entero
 Numero = int (Numero)
-#Se muestra como el tipo de variable cambia aunque se use la misma variable
+#Se muestra el tipo de la variable ya cuando se a cambiado
 print (type(Numero))
-#Se declara un str con meta de sustitucion usando format
+#Se da la salida del mensaje usando format 
 salida = ("El numero utilizado es {}")
 print (salida .format (Numero))
