9274017bc537a9e1fe99e51f4bf5bc20be641a4c
itzel-campos <55063781+itzel-campos@users.noreply.github.com>
2019-09-17 21:52:02 -0500

    Update HolaMundo.py

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,4 +1,4 @@
 #Itzel Campos
 #Fecha de realizacion 15/09/2019
-#Los comentarios se realizan con el #. el resto de la linea no tiene significado para el interprete.
+#Para poder poner comentarios se necesita utilizar el #
 print("Hola Mundo, ¡Ahora en Python!")
