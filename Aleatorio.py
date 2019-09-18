32ba8f02cc025ce4e2774ce1644a448aedbb8884
itzel-campos <55063781+itzel-campos@users.noreply.github.com>
2019-09-17 21:45:07 -0500

    Update Aleatorio.py

--- Aleatorio.py
+++ Aleatorio.py
@@ -1,14 +1,12 @@
 #Itzel Campos
 #16/09/2019
-#Python pose muchas librerias listas para usarse.
-#A dicahs librerias se les da el nombre de modulos.
-#Para que se puedan usar se debe importar con la instruccion import.
+#Python tiene librerias las cuales se pueden utilizar importandolas con la instrucccion import
 import random
 Numero1 = float(10.5)
-#En python una funcio es un conjunto de instrucciones que procesan una tarea especifica, como unidad de ejecucion
-#Se declara con def el codigo que este posicionado a la derecha de la definicion, forma parte de la funcion
+#En python una funcion es un bloque de codigo que hace una tarea especifica tomando en cuenta lo que este alineado
+#a la derecha de la funcion que este se tiene cuando se escribe def
 def Mifuncion ():
-  #Se convierte a float el umero aleatorio generado por random(esto solo es posible si se importa random)
+  #Se cambia a float el numero que es dado por random(esto solo es posible si se importa random)
   Numero2 = float(random.randrange(1,10))
   Mensaje = "La suma de {} y de {} es de {}"
   print(Mensaje.format (Numero1,Numero2,Numero1+Numero2))
