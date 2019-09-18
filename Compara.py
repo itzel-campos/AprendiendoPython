2fd4d7c55f23fa5f963868831b6fbdf9c9d59152
itzel-campos <55063781+itzel-campos@users.noreply.github.com>
2019-09-17 21:48:19 -0500

    Update Compara.py

--- Compara.py
+++ Compara.py
@@ -5,13 +5,11 @@ numero1 = input("Dame el primer numero  ")
 numero2 = input("Dame el segundo numero  ")
 salida = "Numeros proporcionados : {} y {} .{}."
 if (numero1 == numero2):
-    #Entra aqui si los numeros son iguales
+    #Verifica si los numeros son iguales 
     print(salida.format(numero1,numero2,"Los numeros son iguales"))
 else:
-    #condicionales anidadas, if dentro de otro if. Si los numeros no son iguales
+    # Si los numeros no son iguales se pone un if dentro de otro if para hacer las debidas comparaciones 
     if numero1 > numero2:
-        #Reporta si el primer numero es mayor
         print(salida.format(numero1,numero2,"El primer numero es el mayor"))
     else:
-        #O de lo contrario si el primero es menor
         print(salida.format(numero1,numero2,"El segundo numero es el mayor"))
