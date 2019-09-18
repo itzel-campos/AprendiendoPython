5b4f3b98d37ccc4a13bc286a3fc7ceea24695a1b
itzel-campos <55063781+itzel-campos@users.noreply.github.com>
2019-09-17 21:56:01 -0500

    Update Multiplo.py

--- Multiplo.py
+++ Multiplo.py
@@ -1,15 +1,13 @@
 #Itzel Campos
 #16/09/2019
 
-#Se captura un numero y se almacena una vez que es cambiado a int
+#Se pide el numero y se le da un valor entero
 numero = int(input("Dame un numero entero: "))
-#Se almacena en valores booleanos la evaluacion de residuales. Si el residal es cero quiere decir que es multiplo
+#Se compara con el residuo de la divsion para saber si es cero y asi saber si es un multiplo
 esMultiplode3=((numero%3)==0)
 esMultiplode5=((numero%5)==0)
 esMultiplode7=((numero%7)==0)
-#Cuando se usa and, se resuelve por verdadero si todas las condiciones son verdaderas. Cuando se usa or
-#Se resuelve verdadero si al menos una codicion es verdadera
-#Los parentesis le dicen a python que las primeras dos condicione son juntas y la tercera es independiente
+#Los parentesis se usan para indicar qeu las primeras dos comparaciones estan juntas y la tercera es una independiente
 if ((esMultiplode3 and esMultiplode5) or esMultiplode7):
     print("Es correcto")
 else:
